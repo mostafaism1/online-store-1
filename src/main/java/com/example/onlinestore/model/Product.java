7630cb7ad611af3af967fbd5fa8b498113e050df
Mostafa <me.cssign@gmail.com>
2021-01-10 21:36:29 +0200

    Add a product repository

---
+++ src/main/java/com/example/onlinestore/model/Product.java
@@ -0,0 +1,24 @@
+package com.example.onlinestore.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.awt.*;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int Id;
+    private String name;
+    private String description;
+}
