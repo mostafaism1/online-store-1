9977669546c4ecf662918db1803d8e6aaeb403ca
Mostafa <me.cssign@gmail.com>
2021-01-10 20:52:19 +0200

    Add product model

---
+++ src/main/java/com/example/onlinestore/model/product.java
@@ -0,0 +1,12 @@
+package com.example.onlinestore.model;
+
+import lombok.Data;
+
+import java.awt.*;
+
+@Data
+public class product {
+    private int id;
+    private String name;
+    private String description;
+}
