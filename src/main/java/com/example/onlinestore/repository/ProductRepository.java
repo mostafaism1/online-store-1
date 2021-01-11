1b1321d8071d6d30f1171f8aa77e71ee0425047b
Mostafa <me.cssign@gmail.com>
2021-01-11 16:42:13 +0200

    fix imports

--- src/main/java/com/example/onlinestore/repository/ProductRepository.java
+++ src/main/java/com/example/onlinestore/repository/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.example.onlinestore.repository;
 
 import com.example.onlinestore.model.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
