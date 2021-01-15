df20b44a06990e3369a116c34ba4d27d9f519564
Mostafa <me.cssign@gmail.com>
2021-01-15 19:54:47 +0200

    generate schema automatically using hibernate

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO product (name, description) VALUES
 ('product1', 'product1 description'),
 ('product2', 'product2 description');
 
-INSERT INTO user (user_name, password, name, email) VALUES
-('username1', 'pass1', 'name1', 'email1@email.com'),
-('username2', 'pass2', 'name2', 'email2@email.com');
+INSERT INTO user (email, password, name) VALUES
+('email1@email.com', 'pass1', 'name1'),
+('email2@email.com', 'pass2', 'name2');
