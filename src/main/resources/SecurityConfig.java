dabeb54465a69ce40ba2bb25ec10c285d4001da2
Mostafa <me.cssign@gmail.com>
2021-01-15 03:38:34 +0200

    Configure security to use a userDetailsService

---
+++ src/main/resources/SecurityConfig.java
@@ -0,0 +1,27 @@
+import com.example.onlinestore.user.UserRepositoryUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsSerive;
+
+    @Bean 
+    public PasswordEncoderr encoder() { 
+        return new BCryptPasswordEncoder(); 
+    }
+    
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+    }
+
+}
