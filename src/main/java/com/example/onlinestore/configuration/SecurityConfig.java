fb31d26818c6f9905c126543062886a55e798354
Mostafa <me.cssign@gmail.com>
2021-01-15 19:53:34 +0200

    declare dependencies final, and use constructor injection instead of field injection

--- src/main/java/com/example/onlinestore/configuration/SecurityConfig.java
+++ src/main/java/com/example/onlinestore/configuration/SecurityConfig.java
@@ -1,21 +1,26 @@
 import com.example.onlinestore.user.UserRepositoryUserDetailsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    UserDetailsService userDetailsSerive;
+@RequiredArgsConstructor
+class SecurityConfig extends WebSecurityConfigurerAdapter {    
+    
+    private final UserDetailsService userDetailsService;
 
     @Bean 
-    public PasswordEncoderr encoder() { 
+    PasswordEncoder encoder() { 
         return new BCryptPasswordEncoder(); 
     }
     
