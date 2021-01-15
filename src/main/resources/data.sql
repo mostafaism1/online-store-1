29ba53fdb7226aad68a2be8d0c3705532bbc0f37
Mostafa <me.cssign@gmail.com>
2021-01-15 03:33:21 +0200

    add user schema and seed data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,7 @@
-DROP TABLE IF EXISTS product;
-
-CREATE TABLE product (
-                              id INT AUTO_INCREMENT  PRIMARY KEY,
-                              name VARCHAR(50) NOT NULL,
-                              description VARCHAR(250) NOT NULL
-);
-
 INSERT INTO product (name, description) VALUES
 ('product1', 'product1 description'),
 ('product2', 'product2 description');
+
+INSERT INTO user (user_name, password, name, email) VALUES
+('username1', 'pass1', 'name1', 'email1@email.com'),
+('username2', 'pass2', 'name2', 'email2@email.com');
