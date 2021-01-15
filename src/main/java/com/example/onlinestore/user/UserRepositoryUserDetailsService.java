3f0b001ea0ef80fda346b14d863883c39aa6ad37
Mostafa <me.cssign@gmail.com>
2021-01-15 19:47:15 +0200

    declare dependency as final and use lombok to create the constructor

--- src/main/java/com/example/onlinestore/user/UserRepositoryUserDetailsService.java
+++ src/main/java/com/example/onlinestore/user/UserRepositoryUserDetailsService.java
@@ -6,15 +6,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class UserRepositoryUserDetailsService implements UserDetailsService {
 
-    private UserRepository userRepo;
-
-    @Autowired
-    public UserRepositoryUserDetailsService(UserRepository userRepo) {
-        this.userRepo = userRepo;
-    }
+    private final UserRepository userRepo;    
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
