e169d33089ae43e229be7f274a194c90bf24e85a
mostafaism1 <37193984+mostafaism1@users.noreply.github.com>
2021-01-15 19:29:29 +0200

    change the UserDetailsService's access modifier to package-private
    Co-authored-by: Paweł Szymański <pawelfiu@gmail.com>

--- src/main/java/com/example/onlinestore/user/UserRepositoryUserDetailsService.java
+++ src/main/java/com/example/onlinestore/user/UserRepositoryUserDetailsService.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserRepositoryUserDetailsService implements UserDetailsService {
+class UserRepositoryUserDetailsService implements UserDetailsService {
     
     private UserRepository userRepo;
 
