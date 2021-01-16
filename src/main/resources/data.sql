30576fab5f5901dfdaa5f1f68d31c3f48dfa2006
mostafaism1 <37193984+mostafaism1@users.noreply.github.com>
2021-01-16 17:44:01 +0200

    seed db with hashed passwords instead of plain text
    Co-authored-by: Paweł Szymański <pawelfiu@gmail.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,5 +3,5 @@ INSERT INTO product (id, name, description) VALUES
 (2, 'product2', 'product2 description');
 
 INSERT INTO user (email, password, name) VALUES
-('email1@email.com', 'pass1', 'name1'),
-('email2@email.com', 'pass2', 'name2');
+('email1@email.com', '$2a$10$H27RX92/KnQywQOF7l/VCeIa2g77.4pIbldPwrr.Ar8Qw.JXPGokC', 'name1'),
+('email2@email.com', '$2a$10$Lhdy33w8QaV/bJutecweGOrSOAyjUOxz6IAexCtOyRl9ghfsXeWRW', 'name2');
