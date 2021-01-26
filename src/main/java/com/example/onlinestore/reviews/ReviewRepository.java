ecdd744013942b07f1519128e60d7b96972aeb16
mostafa <me.cssign@gmail.com>
2021-01-26 11:55:43 +0200

    set ReviewRepository to package-private

--- src/main/java/com/example/onlinestore/reviews/ReviewRepository.java
+++ src/main/java/com/example/onlinestore/reviews/ReviewRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+interface ReviewRepository extends JpaRepository<Review, Long> {
 
 }
