0167dec1440b5cc43c4de0a06959f11af52c67e9
mostafa <me.cssign@gmail.com>
2021-01-27 02:57:45 +0200

    add @Repository annotation

--- src/main/java/com/example/onlinestore/product/ProductRepository.java
+++ src/main/java/com/example/onlinestore/product/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.example.onlinestore.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
