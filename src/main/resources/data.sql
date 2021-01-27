2e483b509605211bf25d87812c70ab103d40cffa
mostafa <me.cssign@gmail.com>
2021-01-27 02:58:57 +0200

    modify seeded data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,5 +3,5 @@ INSERT INTO product (id, name, description) VALUES
 (2, 'product2', 'product2 description');
 
 INSERT INTO user (email, password, name) VALUES
-('email1@email.com', '$2a$10$H27RX92/KnQywQOF7l/VCeIa2g77.4pIbldPwrr.Ar8Qw.JXPGokC', 'name1'),
-('email2@email.com', '$2a$10$Lhdy33w8QaV/bJutecweGOrSOAyjUOxz6IAexCtOyRl9ghfsXeWRW', 'name2');
+('email1@email.com', '$2a$10$8b18VO9SKZ9UOJCQEsUKi.nuGnxqMEHF/pocLsnpM4lNYZbMrY3DO', 'name1'),
+('email2@email.com', '$2a$10$yP4DR5X1PLe6vHaZBGHvteE0GKuVcxHAuPXxMzD487oq0xya8Uss.', 'name2');
