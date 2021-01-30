97b59e0b85860d151c7ae426a61234e95e97b180
mostafa <me.cssign@gmail.com>
2021-01-30 12:39:55 +0200

    modify seeded data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO product (id, name, description) VALUES
-(1, 'product1', 'product1 description'),
-(2, 'product2', 'product2 description');
+INSERT INTO product (id, name, description, price) VALUES
+(1, 'product1', 'product1 description', 10),
+(2, 'product2', 'product2 description', 20);
 
 INSERT INTO user (email, password, name) VALUES
 ('email1@email.com', '$2a$10$8b18VO9SKZ9UOJCQEsUKi.nuGnxqMEHF/pocLsnpM4lNYZbMrY3DO', 'name1'),
