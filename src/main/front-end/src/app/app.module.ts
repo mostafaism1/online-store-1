00553204bb214519e620fdd4f4088cf15b2aad61
mostafa <me.cssign@gmail.com>
2021-01-30 12:32:31 +0200

    append declarations and imports

--- src/main/front-end/src/app/app.module.ts
+++ src/main/front-end/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import { ProductsComponent } from './store/products/products.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
